e0f523e37944f13e6f27599df7d8d378f021bb2e
Dzuming <dawidpoliszak@op.pl>
2017-07-09 22:39:07 +0200

    Replace firebase method with rest service in schedule effects

--- src/app/state-managements/effects/schedule.effect.ts
+++ src/app/state-managements/effects/schedule.effect.ts
@@ -1,20 +1,22 @@
 import { Effect, Actions, toPayload } from "@ngrx/effects";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { RestService } from "app/shared/services/rest.service";
 
 @Injectable()
 export class ScheduleEffects {
 constructor(
+        private restService: RestService,
         private action$: Actions) { }
 
-    // @Effect() pullArrayFromFirebase$ = this.action$
-    // .ofType('PULL_ARRAY_FROM_FIREBASE')
-    // .switchMap( () => {
-    //     return this.db.list('/schedule')
-    //     .switchMap(result =>
-    //       Observable.of({type: "GOT_FIREBASE_ARRAY", payload: {schedule:  result}})
-    //       )
-    // })
+    @Effect() pullArrayFromFirebase$ = this.action$
+    .ofType('PULL_ARRAY_FROM_FIREBASE')
+    .switchMap( () => {
+        return this.restService.getShedule()
+        .switchMap(result =>
+          Observable.of({type: "GOT_FIREBASE_ARRAY", payload: {schedule:  result}})
+          )
+    })
 
     
 }
