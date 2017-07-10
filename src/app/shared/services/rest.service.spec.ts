db10c6701d66cb14cd56819beaba56c8393393b6
Dzuming <dawidpoliszak@op.pl>
2017-07-10 05:37:05 +0200

    Add tests to save method

--- src/app/shared/services/rest.service.spec.ts
+++ src/app/shared/services/rest.service.spec.ts
@@ -5,13 +5,14 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { RestService } from './rest.service';
 
 describe('RestService', () => {
-  let scheduleObjects: Object[] = [{'12-07-2017': {'Czytanie': 21}, '13-07-2017': {'Czytanie': 11}}];
-  let afDbMock = { 
-                    list: () => Observable.of(scheduleObjects.map(schedule => {return schedule})) 
-                  };
+  let scheduleObjects: Object[] = [{ 'schedule': { '12-07-2017': { 'Czytanie': 21 }, '13-07-2017': { 'Czytanie': 11 } } }];
+  let afDbMock = {
+    list: () => Observable.of(scheduleObjects.map(schedule => { return schedule })),
+  };
   beforeEach(() => {
+
     TestBed.configureTestingModule({
-      providers: [RestService, {"provide": AngularFireDatabase, "useValue": afDbMock}]
+      providers: [RestService, { "provide": AngularFireDatabase, "useValue": afDbMock }]
     });
   });
 
@@ -23,13 +24,22 @@ describe('RestService', () => {
     inject([
       RestService
     ],
-    (restService: RestService) => {
-  let scheduleObs = restService.getShedule();
-  
-  scheduleObs.subscribe(schedule => {
-        expect(schedule.length).toEqual(scheduleObjects.length);
-        expect(schedule[0]).toEqual(scheduleObjects[0]);
-      });
-    })
+      (restService: RestService) => {
+        let scheduleObs = restService.getShedule();
+
+        scheduleObs.subscribe(schedule => {
+          expect(schedule.length).toEqual(scheduleObjects.length);
+          expect(schedule[0]).toEqual(scheduleObjects[0]);
+        });
+      })
+  );
+  it(' save new object to schedule',
+    inject([
+      RestService
+    ],
+      (restService: RestService) => {
+        let object = restService.saveShedule({ 'Czytanie': 3 });
+        expect(object).toEqual(scheduleObjects.length);
+      })
   );
 });
